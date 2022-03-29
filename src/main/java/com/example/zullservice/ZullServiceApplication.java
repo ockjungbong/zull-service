e804bf917239d526cdd6ec96da12fa895910db91
devock <ockjungbong@gmail.com>
2022-03-29 12:12:18 +0900

    first commit

---
+++ src/main/java/com/example/zullservice/ZullServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.zullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullServiceApplication.class, args);
+	}
+
+}
